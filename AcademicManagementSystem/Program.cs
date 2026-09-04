9b4b3bf508986a777127dbb2e876304a4735acf8
MArcSiddiqui <MArcSiddiqui@gmail.com>
2026-09-04 19:53:03 +0500

    route name changed

--- AcademicManagementSystem/Program.cs
+++ AcademicManagementSystem/Program.cs
@@ -27,7 +27,7 @@ namespace AcademicManagementSystem
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=ABC}/{action=Index}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
